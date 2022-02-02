0cb045bf73db4df20f2e6a1c7f87e7d8774e88d1
Mario-F <github@fritschen.net>
2022-02-02 18:42:10 +0100

    feat: setup basic package config
    Signed-off-by: Mario-F <github@fritschen.net>

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+'use strict'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function (parameter: any) {
+  // eslint-disable-next-line no-console
+  console.log(parameter)
+}
